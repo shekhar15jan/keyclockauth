d38eb8ba09f60344063562b679b536dee80a297f
Chandrashekhar Nandanwar <shekhar15jan@gmail.com>
2024-03-20 23:16:47 +0530

    Keycloack with role base api access demo completed

--- src/main/java/com/shekhar/keyclockauth/config/SecurityConfig.java
+++ src/main/java/com/shekhar/keyclockauth/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.shekhar.keyclockauth.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,20 +10,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.GrantedAuthoritiesContainer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Autowired
+    private final JwtAuthConverter jwtAuthConverter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requestMatcher -> requestMatcher.anyRequest().authenticated());
         httpSecurity
-                .oauth2ResourceServer(spec-> spec.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(spec-> {
+                    spec.jwt(jwtConfigurer ->{
+                        Customizer.withDefaults();
+                        jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter);
+                    });
+                });
         httpSecurity.sessionManagement(policy->policy.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
     }
